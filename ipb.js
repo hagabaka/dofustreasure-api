bccaa8c0e09688fa31d688da541aa0bd441341af
Yaohan Chen <yaohan.chen@gmail.com>
2016-07-10 13:57:20 -0400

    Update ipb parser

--- ipb.js
+++ ipb.js
@@ -2,51 +2,50 @@ var cheerio = require('cheerio');
 var get = require('./request');
 
 exports.page = function(url) {
-  var content = get(url);
-
-  var $ = cheerio.load(content);
-  return {
-    posts: function() {
-      return $('.post_block').map(function() {
-        var postingDate = $(this).find('[itemprop=commentTime]').attr('title');
-        return {
-          author: $(this).find('.author_info [itemprop=name]').text(),
-          body: $(this).find('[itemprop=commentText]'),
-          url: $('a[rel="bookmark"]').attr('href'),
-          postingDate: postingDate,
-          editingDate: function() {
-            var dateString = $(this).find('.edit strong').text().replace(/^Edited by [^,]+, (.+)\.$/, '$1');
-            var date = new Date(dateString);
-            if(date.valueOf()) {
-              return dateString;
-            } else {
+  return get(url).then(function(content) {
+    var $ = cheerio.load(content);
+    return {
+      posts: function() {
+        return $('article.cPost').map(function() {
+          var postingDate = $(this).find('a > time').attr('datetime');
+          return {
+            author: $(this).find('.cAuthorPane_author [itemprop=name] a').text(),
+            body: $(this).find('[itemprop=text]'),
+            url: $('a[data-roll="shareComment"]').attr('href'),
+            postingDate: postingDate,
+            editingDate: function() {
+              var dateString = $(this).find('strong > time').attr('datetime');
+              var date = new Date(dateString);
+              if(date.valueOf()) {
+                return dateString;
+              }
               return postingDate;
             }
-          }
+          };
+        }).toArray();
+      },
+      previousPage: function() {
+        return $('.ipsPagination_prev a').attr('href');
+      },
+      nextPage: function() {
+        return $('.ipsPagination_next a').attr('href');
+      },
+      firstPage: function() {
+        return $('.ipsPagination_first a').attr('href') || url;
+      },
+      lastPage: function() {
+        return $('.ipsPagination_last a').attr('href') ||
+          $('.ipsPagination li:last-child a').attr('href') || url;
+      },
+      replyUrl: function() {
+        var upUrl = $('link[rel=up]').attr('href');
+        if(upUrl) {
+          var forumNumber = upUrl.match(/forum\/(\d+)/)[1];
+          var postNumber = url.match(/topic\/(\d+)/)[1];
+          var baseUrl = url.match(/(.+)\/topic\/.+/)[1];
+          return baseUrl + '/index.php?app=forums&module=post&section=post&do=reply_post&f=' + forumNumber + '&t=' + postNumber;
         }
-      }).toArray();
-    },
-    previousPage: function() {
-      return $('.topic_controls .prev a').attr('href');
-    },
-    nextPage: function() {
-      return $('.topic_controls .next a').attr('href');
-    },
-    firstPage: function() {
-      return $('.topic_controls .pages li:nth-child(2) a').attr('href') || url;
-    },
-    lastPage: function() {
-      return $('.topic_controls .forward li.last a').attr('href') ||
-             $('.topic_controls .pages li:last-child a').attr('href') || url;
-    },
-    replyUrl: function() {
-      var upUrl = $('link[rel=up]').attr('href');
-      if(upUrl) {
-        var forumNumber = upUrl.match(/forum\/(\d+)/)[1];
-        var postNumber = url.match(/topic\/(\d+)/)[1];
-        var baseUrl = url.match(/(.+)\/topic\/.+/)[1];
-        return baseUrl + '/index.php?app=forums&module=post&section=post&do=reply_post&f=' + forumNumber + '&t=' + postNumber;
       }
-    }
-  }
-}
+    };
+  });
+};
